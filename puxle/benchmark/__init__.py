8418f185396a8ec3f79ce9162cd02b1a4cedff2e
tinker495 <wjdrbtjr495@gmail.com>
2025-11-23 21:11:44 +0900

    add santa benchmarks

--- puxle/benchmark/__init__.py
+++ puxle/benchmark/__init__.py
@@ -1,6 +1,20 @@
 from .benchmark import Benchmark, BenchmarkSample
 from .lightsout_deepcubea import LightsOutDeepCubeABenchmark
 from .rubikscube_deepcubea import RubiksCubeDeepCubeABenchmark, RubiksCubeDeepCubeAHardBenchmark
+from .rubikscube_santa import (
+    RubiksCubeSantaBenchmark,
+    RubiksCubeSantaRandomBenchmark,
+    RubiksCubeSanta222Benchmark,
+    RubiksCubeSanta333Benchmark,
+    RubiksCubeSanta444Benchmark,
+    RubiksCubeSanta555Benchmark,
+    RubiksCubeSanta666Benchmark,
+    RubiksCubeSantaRandom222Benchmark,
+    RubiksCubeSantaRandom333Benchmark,
+    RubiksCubeSantaRandom444Benchmark,
+    RubiksCubeSantaRandom555Benchmark,
+    RubiksCubeSantaRandom666Benchmark,
+)
 from .slidepuzzle_deepcubea import SlidePuzzleDeepCubeABenchmark, SlidePuzzleDeepCubeA15Benchmark, SlidePuzzleDeepCubeA15HardBenchmark, SlidePuzzleDeepCubeA24Benchmark, SlidePuzzleDeepCubeA35Benchmark, SlidePuzzleDeepCubeA48Benchmark
 
 # All benchmark implementations
@@ -10,6 +24,18 @@ __all__ = [
     "LightsOutDeepCubeABenchmark",
     "RubiksCubeDeepCubeABenchmark",
     "RubiksCubeDeepCubeAHardBenchmark",
+    "RubiksCubeSantaBenchmark",
+    "RubiksCubeSantaRandomBenchmark",
+    "RubiksCubeSanta222Benchmark",
+    "RubiksCubeSanta333Benchmark",
+    "RubiksCubeSanta444Benchmark",
+    "RubiksCubeSanta555Benchmark",
+    "RubiksCubeSanta666Benchmark",
+    "RubiksCubeSantaRandom222Benchmark",
+    "RubiksCubeSantaRandom333Benchmark",
+    "RubiksCubeSantaRandom444Benchmark",
+    "RubiksCubeSantaRandom555Benchmark",
+    "RubiksCubeSantaRandom666Benchmark",
     "SlidePuzzleDeepCubeABenchmark",
     "SlidePuzzleDeepCubeA15Benchmark",
     "SlidePuzzleDeepCubeA15HardBenchmark",
