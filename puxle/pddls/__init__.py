81270c98e759834981536a90fb0ce512ab7d3eb0
tinker495 <wjdrbtjr495@gmail.com>
2026-07-02 11:23:30 +0900

    fix: avoid pddl facade import cycle

--- puxle/pddls/__init__.py
+++ puxle/pddls/__init__.py
@@ -1,3 +1,15 @@
-from .pddl import PDDL
+from __future__ import annotations
+
+from puxle._lazy_imports import lazy_dir, load_lazy_export
 
 __all__ = ["PDDL"]
+
+_EXPORTS = {"PDDL": (".pddl", "PDDL")}
+
+
+def __getattr__(name: str):
+    return load_lazy_export(name, __name__, _EXPORTS, globals())
+
+
+def __dir__() -> list[str]:
+    return lazy_dir(globals(), __all__)
